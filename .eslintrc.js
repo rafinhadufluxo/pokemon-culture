3d1bd18a2a5ac98f9d77524ba67c12ad29acced4
rafinhadufluxo <arruda.rafa.ra@gmail.com>
2024-07-07 14:53:02 -0400

    add config para gitpages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,3 @@
-# pokemon-culture
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Don't forget to put .eslint in the root of the project, copy this code below
-```
 module.exports = {
     root: true,
     env: {
@@ -41,9 +17,5 @@ module.exports = {
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     },
-};
+  };
   
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
